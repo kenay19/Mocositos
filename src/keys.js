ef151c0a0a55cc2522b5d503afdb3341e46ea338
Kenay <omarlazor@hotmail.com>
2022-02-28 22:55:50 -0600

    six commit

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    
+    database : {
+        
+        host : 'localhost' ,
+        user: 'root',
+        password: '',
+        database: 'Mocositos'
+
+    }
+
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const con = require('../database');
 
 /**
  *  Pagina inicial de la aplicacion 
@@ -9,4 +10,11 @@ router.get('/' ,(req,res) => {
     res.render('index');
 });
 
+/**
+ * Verifica si hay sesion si la hay regresa un dato 
+ */
+router.post('/', async (req,res) => {
+    let result = await pool.query('SELECT * FROM Usuario WHERE ?',[req.body]);
+    console.log(result);
+});
 module.exports = router;
