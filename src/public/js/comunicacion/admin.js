226d571a397bb286a349a87eb4833be462679337
Ernesto071101 <ernestomartinezislas.1290@gmail.com>
2022-03-08 20:26:50 -0600

    FormularioAdmin

--- src/public/js/comunicacion/admin.js
+++ src/public/js/comunicacion/admin.js
@@ -5,9 +5,26 @@ document.getElementById('save').addEventListener('click',(e)=>{
         url: '/admin/insert',
         type: 'POST',
         dataType: 'json',
-        data: formularioAdmin,
+        data:
+        {
+           rfc : formularioAdmin['rfc'].value,
+           curp: formularioAdmin['curp'].value,
+           cedulaProfesional: formularioAdmin['cedulaProfecional'].value,
+           especialidad: formularioAdmin['especialidad'].value,
+           nombre: formularioAdmin['nombre'].value,
+           app: formularioAdmin['app'].value,
+           apm: formularioAdmin['apm'].value,
+           tel : formularioAdmin['tel'].value,
+           calle: formularioAdmin['calle'].value,
+           inte: formularioAdmin['inte'].value,
+           exte: formularioAdmin['exte'].value,
+           colonia: formularioAdmin['colonia'].value,
+           municipio: formularioAdmin['municipio'].value,
+           estado: formularioAdmin['estado'].value,
+           cp: formularioAdmin['cp'].value,
+        },
         success: (data)=>{
-            console.log(data);
-        }
+            console.log();
+        }//success
     });
 });
