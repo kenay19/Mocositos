a7c8d5ea84796f770caf1ef7f603137328034412
Ernesto071101 <ernestomartinezislas.1290@gmail.com>
2022-03-10 10:34:37 -0600

    sdfgdf

--- src/public/js/comunicacion/updateMedic.js
+++ src/public/js/comunicacion/updateMedic.js
@@ -1,6 +1,3 @@
-
-
-
 window.onload = () => {
     $.ajax({
         url: 'admin/modify',
@@ -11,4 +8,45 @@ window.onload = () => {
             console.log(data);
         }
     })
-}
+};
+
+document.getElementById('update').addEventListener('click',(e) => {
+    const formularioUpdate = document.forms['form-update'];
+    let tipo;
+    if (formularioUpdate['AdminGender'].value == 'Pediatria')
+    {
+        tipo = 'solicitante';
+    }
+    else
+    {
+        tipo = 'tecnico'
+    }
+    $.ajax({
+        url: '/admin/modify/' + formularioUpdate['idMedico'].value,
+        type: "PUT",
+        dataType: "json",
+        data:
+        {
+           tipo,
+           email: formularioUpdate['email'].value,
+           rfc : formularioUpdate['rfc'].value,
+           curp: formularioUpdate['curp'].value,
+           cedulaProfesional: formularioUpdate['cedulaProfesional'].value,
+           especialidad: formularioUpdate['AdminGender'].value,
+           nombre: formularioUpdate['nombre'].value,
+           app: formularioUpdate['app'].value,
+           apm: formularioUpdate['apm'].value,
+           telefono : formularioUpdate['telefono'].value,
+           calle: formularioUpdate['calle'].value,
+           inte: formularioUpdate['inte'].value,
+           exte: formularioUpdate['exte'].value,
+           colonia: formularioUpdate['colonia'].value,
+           municipio: formularioUpdate['municipio'].value,
+           estado: formularioUpdate['estado'].value,
+           cp: formularioUpdate['cp'].value,
+        },
+        success: (data) => {
+            
+        }
+   });
+});
